f950f856fbd655a8e163a6e8e169e0722743b509
JWLK <utxtion@me.com>
2021-02-22 12:44:19 +0900

    Initial commit

---
+++ app/src/test/java/com/jwlks/localdblistview/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.jwlks.localdblistview;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
